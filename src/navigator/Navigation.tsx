0adc81560964d1ac574ed0ec276ecc286a3f9614
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-04 10:37:46 -0600

    loginscreen in navigator added

--- src/navigator/Navigation.tsx
+++ src/navigator/Navigation.tsx
@@ -1,9 +1,11 @@
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {WelcomeScreen} from '../screens/WelcomeScreen';
+import {LoginScreen} from '../screens/LoginScreen';
 
 export type RootStackParams = {
   WelcomeScreen: undefined;
+  LoginScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -11,7 +13,8 @@ const Stack = createStackNavigator<RootStackParams>();
 export const Navigation = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+      {/* <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} /> */}
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
     </Stack.Navigator>
   );
 };
