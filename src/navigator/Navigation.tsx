ac804dbe0a60d741f431b5a53c28c5fdbb4c1c9e
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-06 09:53:38 -0600

    signup screen added

--- src/navigator/Navigation.tsx
+++ src/navigator/Navigation.tsx
@@ -2,10 +2,12 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import {WelcomeScreen} from '../screens/WelcomeScreen';
 import {LoginScreen} from '../screens/LoginScreen';
+import {SignUpScreen} from '../screens/SignUpScreen';
 
 export type RootStackParams = {
   WelcomeScreen: undefined;
   LoginScreen: undefined;
+  SignUpScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -14,7 +16,8 @@ export const Navigation = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       {/* <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} /> */}
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      {/* <Stack.Screen name="LoginScreen" component={LoginScreen} /> */}
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
     </Stack.Navigator>
   );
 };
