4aba31f7384e49903cb0074cc59b60a3d78fac65
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-07 07:56:19 -0600

    screen added

---
+++ src/screens/OptionsUnLoggedScreen.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {View, StyleSheet, SafeAreaView, Text} from 'react-native';
+
+import {BackButtonBar} from '../components/BackButtonBar';
+import {SubmitButton} from '../components/SubmitButton';
+
+import UserTickIcon from '../assets/user_tick.svg';
+
+import {colors} from '../styles/colors';
+
+export const OptionsUnLoggedScreen = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: colors.PrimaryColor}}>
+      <BackButtonBar titleText="Mis Opciones" primaryColorDefault={false} />
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <UserTickIcon height={100} />
+        </View>
+        <View>
+          <Text style={styles.primaryText}>Inicia sesión o </Text>
+          <Text style={styles.primaryText}>registrate</Text>
+          <Text style={styles.secondaryText}>
+            y vive una nueva experiencia de
+          </Text>
+          <Text style={styles.secondaryText}>compra en ferreterias</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <SubmitButton
+          textButton="Comenzar"
+          customStyles={{backgroundColor: colors.SecondaryColor}}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.PrimaryColor,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 35,
+  },
+  imageContainer: {
+    marginBottom: 20,
+  },
+  primaryText: {
+    textAlign: 'left',
+    color: colors.SecondaryTextColor,
+    fontSize: 24,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  secondaryText: {
+    color: colors.White,
+    fontSize: 14,
+    fontFamily: 'Poppins-Regular',
+  },
+  buttonContainer: {
+    marginBottom: 20,
+    paddingHorizontal: 35,
+  },
+});
