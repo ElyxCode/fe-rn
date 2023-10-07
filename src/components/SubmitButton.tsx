234a0af3ae16f384c4a3a863c5eb72323467a3fb
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-07 07:52:22 -0600

    custom style prop added

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,18 +1,26 @@
-import {TouchableHighlight, View, Text, StyleSheet} from 'react-native';
+import {
+  TouchableHighlight,
+  View,
+  Text,
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
 import {colors} from '../styles/colors';
 
 type Props = {
   textButton: string;
+  customStyles?: StyleProp<ViewStyle>;
 };
 
-export const SubmitButton = ({textButton}: Props) => {
+export const SubmitButton = ({textButton, customStyles}: Props) => {
   return (
     <TouchableHighlight
       underlayColor={colors.White}
       onPress={() => {
         console.log('Presione el boton');
       }}>
-      <View style={styles.buttonContainer}>
+      <View style={[styles.buttonContainer, customStyles]}>
         <Text style={styles.textButton}>{textButton}</Text>
       </View>
     </TouchableHighlight>
@@ -21,8 +29,8 @@ export const SubmitButton = ({textButton}: Props) => {
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    backgroundColor: colors.PrimaryColor,
     justifyContent: 'center',
+    backgroundColor: colors.PrimaryColor,
     height: 53,
     borderRadius: 10,
   },
