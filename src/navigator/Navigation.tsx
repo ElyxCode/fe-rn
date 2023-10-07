7254f3421a44f49aa0d575d0cca49732ed32af8b
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-07 08:17:10 -0600

    initial homebranch screen ui

--- src/navigator/Navigation.tsx
+++ src/navigator/Navigation.tsx
@@ -4,12 +4,14 @@ import {WelcomeScreen} from '../screens/WelcomeScreen';
 import {LoginScreen} from '../screens/LoginScreen';
 import {SignUpScreen} from '../screens/SignUpScreen';
 import {OptionsUnLoggedScreen} from '../screens/OptionsUnLoggedScreen';
+import {HomeBranchScreen} from '../screens/HomeBranchScreen';
 
 export type RootStackParams = {
   WelcomeScreen: undefined;
   LoginScreen: undefined;
   SignUpScreen: undefined;
   OptionsUnLoggedScreen: undefined;
+  HomeBranchScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -20,10 +22,11 @@ export const Navigation = () => {
       {/* <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} /> */}
       {/* <Stack.Screen name="LoginScreen" component={LoginScreen} /> */}
       {/* <Stack.Screen name="SignUpScreen" component={SignUpScreen} /> */}
-      <Stack.Screen
+      {/* <Stack.Screen
         name="OptionsUnLoggedScreen"
         component={OptionsUnLoggedScreen}
-      />
+      /> */}
+      <Stack.Screen name="HomeBranchScreen" component={HomeBranchScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/HomeBranchScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+export const HomeBranchScreen = () => {
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+};
