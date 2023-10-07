5e87d95d2218273f3522f1effacaa9b94692c78f
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-07 09:17:58 -0600

    components added

--- src/screens/HomeBranchScreen.tsx
+++ src/screens/HomeBranchScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import {CustomNavBarHome} from '../components/CustomNavBarHome';
+import {LocationBar} from '../components/LocationBar';
 
 export const HomeBranchScreen = () => {
   return (
-    <SafeAreaView>
-      <View></View>
+    <SafeAreaView style={{flex: 1}}>
+      <CustomNavBarHome />
+      <LocationBar />
+      <ScrollView style={styles.scrollContainer}></ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    backgroundColor: 'red',
+  },
+});
