79132042fbb28b1bbee67b94b6b497e05d79e055
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-08 10:35:07 -0600

    category list, promo list and branches list added

--- src/screens/HomeBranchScreen.tsx
+++ src/screens/HomeBranchScreen.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+
 import {CustomNavBarHome} from '../components/CustomNavBarHome';
 import {LocationBar} from '../components/LocationBar';
+import {PromoList} from '../components/PromoList';
+import {CategoryHomeList} from '../components/CategoryHomeList';
+import {BranchHomeList} from '../components/BranchHomeList';
 
 export const HomeBranchScreen = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <CustomNavBarHome />
       <LocationBar />
-      <ScrollView style={styles.scrollContainer}></ScrollView>
+      <ScrollView style={styles.scrollContainer}>
+        <PromoList />
+        <CategoryHomeList />
+        <BranchHomeList />
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -18,6 +26,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollContainer: {
-    backgroundColor: 'red',
+    paddingHorizontal: 20,
   },
 });
