1764ed342970cbbb28389f5dc7a4e12682925572
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-08 10:34:20 -0600

    initial branches list component added

---
+++ src/components/BranchHomeList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const BranchItemRender = () => {
+  return <View></View>;
+};
+
+export const BranchHomeList = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
