05f21dc137812571fd5bf04e84f4ed45a3dc6a36
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-17 11:24:31 -0600

    change name services

--- src/services/branch.ts
+++ src/services/branch.ts
@@ -2,6 +2,6 @@ import { ApiResponse } from "apisauce";
 import { api } from "../api/api-config"
 import { Branch } from '../model/Branch';
 
-export const BranchService = async (lat: string = '13.8263447', lng: String = '-89.270477') => {
+export const branchService = async (lat: string = '13.8263447', lng: String = '-89.270477') => {
     return api.get<Branch[]>('/branchs', { lat: lat, lng: lng })
 };
