e91ff12e197f98b9d51f3bd766bb861b23da2ece
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-17 14:15:31 -0600

    selected tag category item style added

--- src/components/CategoryHomeList.tsx
+++ src/components/CategoryHomeList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, Image, Pressable, StyleSheet, Text, View} from 'react-native';
 
 import {Category} from '../model/Category';
 
@@ -11,34 +11,55 @@ type CategoryHomeListProps = {
   categories: Category[];
 };
 
-const CategoryItemRender = ({
-  imageUri,
-  category,
-}: {
-  imageUri?: string;
-  category?: string;
-}) => {
-  return (
-    <View style={styles.categoryContainer}>
-      {imageUri === 'allCategory' ? (
-        <AllCategory height={17} width={21} />
-      ) : (
-        <Image source={{uri: imageUri}} height={17} width={21} />
-      )}
-      <Text style={styles.categoryText}>{category}</Text>
-    </View>
-  );
-};
-
 export const CategoryHomeList = ({categories}: CategoryHomeListProps) => {
+  const [categoryId, setCategoryId] = useState<string | null>(null);
+
+  const handleSelectionItem = (id: string) => {
+    let selectedItemId = categoryId;
+    if (selectedItemId === id) {
+      setCategoryId(null);
+    } else {
+      setCategoryId(id);
+    }
+  };
+
+  const CategoryItemRender = ({
+    id,
+    imageUri,
+    category,
+  }: {
+    id: string;
+    imageUri?: string;
+    category?: string;
+  }) => {
+    return (
+      <Pressable onPress={() => handleSelectionItem(id)}>
+        <View
+          style={[
+            styles.categoryContainer,
+            id === categoryId ? styles.categoryContainerSelected : null,
+          ]}>
+          {imageUri === 'allCategory' ? (
+            <AllCategory height={17} width={21} />
+          ) : (
+            <Image source={{uri: imageUri}} height={17} width={21} />
+          )}
+          <Text style={styles.categoryText}>{category}</Text>
+        </View>
+      </Pressable>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>Todo en un solo lugar</Text>
       <FlatList
         horizontal={true}
         data={categories}
+        extraData={categoryId}
         renderItem={({item}) => (
           <CategoryItemRender
+            id={item.id.toString()}
             imageUri={item.avatar?.toString()}
             category={item.name}
           />
@@ -69,6 +90,10 @@ const styles = StyleSheet.create({
     height: 40,
     paddingHorizontal: 12,
     columnGap: 8,
+  },
+  categoryContainerSelected: {
+    borderColor: colors.SecondaryColor,
+    borderWidth: 1,
   },
   categoryText: {
     fontSize: 12,
