662e0740613f954284599304b3927fc1975f052c
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-18 11:09:18 -0600

    use react hook form and basic logic for normal login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet, Pressable} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Alert,
+} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
 
 import {CustomTextInput} from '../components/CustomTextInput';
 import {SubmitButton} from '../components/SubmitButton';
@@ -13,66 +21,144 @@ import GoogleLogoIcon from '../assets/google_logo.svg';
 import AppleLogoIcon from '../assets/apple_logo.svg';
 
 import {colors} from '../styles/colors';
+import {loginServices} from '../services/auth';
+import {User} from '../model/User';
+import {LoaderScreen} from './LoaderScreen';
+import Messages from '../constants/Messages';
 
 export const LoginScreen = ({navigation}: any) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleOnSubmit = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    setIsLoading(true);
+    const response = await loginServices(email, password);
+    if (response.ok) {
+      console.log({user: response.data?.user});
+      navigation.navigate('UserOptionsMenuScreen');
+    } else {
+      console.log({error: response.data?.error});
+      Alert.alert('Ferreplace', response.data?.error, [{text: 'Aceptar'}]);
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleOnError = (errors: any) => {
+    if (errors.email) {
+      return Alert.alert('Ferreplace', errors.email.message, [
+        {text: 'Aceptar'},
+      ]);
+    }
+
+    if (errors.password) {
+      Alert.alert(Messages.titleMessage, errors.password.message, [
+        {text: Messages.okButton},
+      ]);
+    }
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
-      <CustomNavBar />
-      <View style={styles.container}>
-        <UserTickIcon height={'75'} fill={colors.DarkGrayColor} />
-        <Text style={styles.titleText}>Inicia sesión</Text>
-        <View style={styles.descriptionContainer}>
-          <Text style={styles.descriptionText}>
-            y vive una nueva experiencia de{' '}
-          </Text>
-          <Text style={styles.descriptionText}>compra en ferreteria</Text>
-        </View>
-        <View style={styles.inputTextContainer}>
-          <CustomTextInput
-            InputIcon={SMSTrackingIcon}
-            placeHolder="Correo Electrónico"
-            fill={colors.PrimaryColor}
-          />
-          <CustomTextInput
-            InputIcon={LockIcon}
-            placeHolder="Contraseña"
-            isPassword={true}
-          />
-        </View>
-        <Text style={styles.recoverPassword}>Recuperar Contraseña</Text>
-        <View style={styles.buttonsContainer}>
-          <SubmitButton
-            textButton="Iniciar Sesión"
-            onPress={() => navigation.navigate('UserOptionsMenuScreen')}
-          />
-          <ThirdPartyButton
-            textButton="Continuar con Google"
-            ButtonIcon={GoogleLogoIcon}
-          />
-          <ThirdPartyButton
-            textButton="Continuar con Apple"
-            ButtonIcon={AppleLogoIcon}
-            customBackgroundColor={colors.Black}
-            customTextColor={colors.White}
-          />
-        </View>
-        <View
-          style={{
-            alignItems: 'center',
-            marginTop: 15,
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}>
-          <Text style={styles.noAccountMessage}>
-            ¿Aún no tienes una cuenta?
-          </Text>
-          <Pressable onPress={() => navigation.navigate('SignUpScreen')}>
-            <Text style={[styles.noAccountMessage, styles.registerMessage]}>
-              Registrarme
-            </Text>
-          </Pressable>
-        </View>
-      </View>
+      {isLoading ? (
+        <LoaderScreen />
+      ) : (
+        <>
+          <CustomNavBar />
+          <View style={styles.container}>
+            <UserTickIcon height={'75'} fill={colors.DarkGrayColor} />
+            <Text style={styles.titleText}>Inicia sesión</Text>
+            <View style={styles.descriptionContainer}>
+              <Text style={styles.descriptionText}>
+                y vive una nueva experiencia de{' '}
+              </Text>
+              <Text style={styles.descriptionText}>compra en ferreteria</Text>
+            </View>
+            <View style={styles.inputTextContainer}>
+              <Controller
+                control={control}
+                rules={{required: Messages.requireEmailMessage}}
+                render={({field: {onChange, value, onBlur}}) => (
+                  <CustomTextInput
+                    InputIcon={SMSTrackingIcon}
+                    placeHolder="Correo Electrónico"
+                    fill={colors.PrimaryColor}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                  />
+                )}
+                name="email"
+              />
+
+              <Controller
+                control={control}
+                rules={{required: Messages.requirePasswordMessage}}
+                render={({field: {onChange, value, onBlur}}) => (
+                  <CustomTextInput
+                    InputIcon={LockIcon}
+                    placeHolder="Contraseña"
+                    isPassword={true}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                  />
+                )}
+                name="password"
+              />
+            </View>
+            <Text style={styles.recoverPassword}>Recuperar Contraseña</Text>
+            <View style={styles.buttonsContainer}>
+              <SubmitButton
+                textButton="Iniciar Sesión"
+                onPress={handleSubmit(handleOnSubmit, handleOnError)}
+              />
+              <ThirdPartyButton
+                textButton="Continuar con Google"
+                ButtonIcon={GoogleLogoIcon}
+              />
+              <ThirdPartyButton
+                textButton="Continuar con Apple"
+                ButtonIcon={AppleLogoIcon}
+                customBackgroundColor={colors.Black}
+                customTextColor={colors.White}
+              />
+            </View>
+            <View
+              style={{
+                alignItems: 'center',
+                marginTop: 15,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <Text style={styles.noAccountMessage}>
+                ¿Aún no tienes una cuenta?
+              </Text>
+              <Pressable onPress={() => navigation.navigate('SignUpScreen')}>
+                <Text style={[styles.noAccountMessage, styles.registerMessage]}>
+                  Registrarme
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
 };
