4347226cbd443fac159d368cc0a8852665a4586a
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-18 17:53:39 -0600

    recoil remove

--- src/navigator/ProfileNavigation.tsx
+++ src/navigator/ProfileNavigation.tsx
@@ -5,8 +5,6 @@ import {SignUpScreen} from '../screens/SignUpScreen';
 import {OptionsUnLoggedScreen} from '../screens/OptionsUnLoggedScreen';
 import {UserOptionsMenuScreen} from '../screens/UserOptionsMenuScreen';
 import {SettingsScreen} from '../screens/SettingsScreen';
-import {useRecoilState} from 'recoil';
-import {tokenState} from '../utils/store';
 
 export type ProfileStackParams = {
   LoginScreen: undefined;
@@ -19,30 +17,20 @@ export type ProfileStackParams = {
 const ProfileStack = createStackNavigator<ProfileStackParams>();
 
 export const ProfileNavigation = () => {
-  const [token] = useRecoilState(tokenState);
   return (
     <ProfileStack.Navigator screenOptions={{headerShown: false}}>
-      {token ? (
-        <>
-          <ProfileStack.Screen
-            name="UserOptionsMenuScreen"
-            component={UserOptionsMenuScreen}
-          />
-          <ProfileStack.Screen
-            name="SettingsScreen"
-            component={SettingsScreen}
-          />
-        </>
-      ) : (
-        <>
-          <ProfileStack.Screen
-            name="OptionsUnLoggedScreen"
-            component={OptionsUnLoggedScreen}
-          />
-          <ProfileStack.Screen name="LoginScreen" component={LoginScreen} />
-          <ProfileStack.Screen name="SignUpScreen" component={SignUpScreen} />
-        </>
-      )}
+      <ProfileStack.Screen
+        name="UserOptionsMenuScreen"
+        component={UserOptionsMenuScreen}
+      />
+      <ProfileStack.Screen name="SettingsScreen" component={SettingsScreen} />
+
+      <ProfileStack.Screen
+        name="OptionsUnLoggedScreen"
+        component={OptionsUnLoggedScreen}
+      />
+      <ProfileStack.Screen name="LoginScreen" component={LoginScreen} />
+      <ProfileStack.Screen name="SignUpScreen" component={SignUpScreen} />
     </ProfileStack.Navigator>
   );
 };
