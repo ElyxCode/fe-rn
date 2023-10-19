82dd63a1c8a9eeacc595879116ad41c531b6a570
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-19 12:05:38 -0600

    redux persist config

--- App.tsx
+++ App.tsx
@@ -1,25 +1,24 @@
 import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 
+import {PersistGate} from 'redux-persist/integration/react';
+
 import {Provider} from 'react-redux';
 
-import {store} from './src/utils/store';
+import {persistor, store} from './src/utils/store';
 
 import SplashScreen from 'react-native-splash-screen';
 
 import {NavigationContainer} from '@react-navigation/native';
 
-import {Navigation} from './src/navigator/Navigation';
 import Geocoder from 'react-native-geocoding';
 import {enableLatestRenderer} from 'react-native-maps';
-
 
 import {MainNavigation} from './src/navigator/MainNavigation';
 
 const App = (): JSX.Element => {
-
   enableLatestRenderer();
-  Geocoder.init("AIzaSyCytD23EG5zvcDjToXFyAYnvWcVd-e0ETw");
+  Geocoder.init('AIzaSyCytD23EG5zvcDjToXFyAYnvWcVd-e0ETw');
 
   useEffect(() => {
     SplashScreen.hide();
@@ -27,9 +26,11 @@ const App = (): JSX.Element => {
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <MainNavigation />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,13 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import authReducer from '../services/auth/authSlice';
 import userReducer from '../services/user/userSlice';
 
-export const store = configureStore({
-  reducer: {
+// adding our persist configs
+export const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: AsyncStorage,
+
+};
+
+  // adding our rootReducer
+const rootReducer = combineReducers({
     authToken: authReducer,
-    user: userReducer
-  },
+    user: userReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
