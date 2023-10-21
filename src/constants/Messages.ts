bc22e118d5301bd33fd9ee08fae7f7327b5a42e6
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-21 11:12:25 -0600

    new message added

--- src/constants/Messages.ts
+++ src/constants/Messages.ts
@@ -4,5 +4,11 @@ export default {
     cancelButton: 'Cancelar',
     requireEmailMessage: 'Correo electrónico es requerido',
     requirePasswordMessage: 'Contraseña es requerida',
+    requireNameProfile: 'Debe colocar su nombre',
+    requireEmailProfile: 'Debe colocar su correo electronico',
+    requireBirthDayProfile: 'Debe colocar su fecha de nacimiento',
+    requireRoleProfile: 'Debe elegir un rol',
+    requireDuiProfile: 'Debe ingresar su DUI',
+    requireFiscalNumber: 'Debe ingresar su numero fiscal',
 
 }
