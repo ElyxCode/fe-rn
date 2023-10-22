da82355cfaa02c6c9547453314b0704ba606e680
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-22 11:05:57 -0600

    transform UTC date to DDMMYYYY format

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,5 +1,15 @@
-export const transformBirthDate = (dateFormat: string): string => {
+export const transformBirthDateToSend = (dateFormat: string): string => {
     let date: string[] = dateFormat.split('/');
     // console.log(date[2] + '-' + date[1] + '-' + date[0]);
     return date[2] + '-' + date[1] + '-' + date[0];
+};
+
+export const transformBirthDateUTCTtoDDMMYYYY = (dateFormat: string): string => {
+    if(dateFormat === '') return '';
+    let date: string[] = dateFormat.split('T');  
+    let dateT: string[] = date[0].split('-');    
+    let year: string = dateT[0];
+    let month: string = dateT[1];
+    let day: string = dateT[2];
+    return day + '/' + month+ '/' + year;
 };
