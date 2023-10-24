4ed5f10a2740050be87afa83094d6d3d762b0067
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-24 13:34:09 -0600

    change typing services

--- src/services/promotion.ts
+++ src/services/promotion.ts
@@ -2,6 +2,6 @@ import { ApiResponse } from "apisauce"
 import { api } from "../api/api-config"
 import { Promotion } from "../model/Promotion"
 
-export const promotionServices = async (lat: string = '13.8263447', lng: string = '-89.270477', type: string = 'normal'):Promise<ApiResponse<Promotion[]>> => {
-    return api.get('/promos')
+export const promotionServices = async (lat: string = '13.8263447', lng: string = '-89.270477', type: string = 'normal'): Promise<ApiResponse<Promotion[]>> => {
+    return api.get('/promos', { lat: lat, lng: lng, type: type })
 }
