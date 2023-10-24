4d5aa75b4b1dc2c8276f66571f3a7733668e9ae1
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-10-24 17:19:07 -0600

    get promos by branch

--- src/services/promotion.ts
+++ src/services/promotion.ts
@@ -5,3 +5,7 @@ import { Promotion } from "../model/Promotion"
 export const promotionServices = async (lat: string = '13.8263447', lng: string = '-89.270477', type: string = 'normal'): Promise<ApiResponse<Promotion[]>> => {
     return api.get('/promos', { lat: lat, lng: lng, type: type })
 }
+
+export const promotionByBranchServices = async (lat: string|number = '13.8263447', lng: string|number = '-89.270477', type: string = 'normal', branchId: string): Promise<ApiResponse<Promotion[]>> => {
+    return api.get('/promos', { lat: lat, lng: lng, type: type, branch_id: branchId })
+}
