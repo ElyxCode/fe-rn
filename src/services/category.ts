4fc64d10d7d479fbe5ae63b8aa2c7340b200a1c1
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-11-01 10:00:09 -0600

    get category by branch services

--- src/services/category.ts
+++ src/services/category.ts
@@ -6,3 +6,7 @@ export const categoryServices = async (): Promise<ApiResponse<Category[]>> => {
     return api.get('/categories');
 }
 
+export const categoryByBranchServices = async (branchId: string): Promise<ApiResponse<Category[]>> => {
+    return api.get('/categories', { branch_id: branchId});
+}
+
