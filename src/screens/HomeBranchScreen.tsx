78fe36291dfecccf927cf1a3ee5a0c0c692da3c9
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-11-05 09:13:54 -0600

    safe area provider and fix safe area bug with navigation modal

--- src/screens/HomeBranchScreen.tsx
+++ src/screens/HomeBranchScreen.tsx
@@ -1,11 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  View,
-} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 import {useAppSelector} from '../hooks/useRedux';
 
