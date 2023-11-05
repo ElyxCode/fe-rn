870907a01bbcf75e9f80e3f5bc7bc978243a11c4
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-11-05 09:51:19 -0600

    if social login prop added and logout google if google signin

--- src/services/auth/authSlice.ts
+++ src/services/auth/authSlice.ts
@@ -2,23 +2,32 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { persistConfig } from '../../utils/store'
 
+export const thirdPartySocial = {
+    google: 'google',
+    apple: 'apple'
+}
+
 type AuthToken = {
     token: string;
+    social?: string;
 }
 
 const initialState: AuthToken = {
     token: '',
+    social: ''
 };
 
 export const authSlice = createSlice({
     name: 'token',
     initialState: initialState,
     reducers: {
-        setToken: (state, action: PayloadAction<string>) => {
-            state.token = action.payload
+        setToken: (state, action: PayloadAction<AuthToken>) => {
+            state.token = action.payload.token,         
+            state.social = action.payload.social ?? initialState.social
         },
         clearToken: (state) => {
-            state.token = '';
+           state.token = initialState.token;
+           state.social = initialState.social;
         }
     },
   });
