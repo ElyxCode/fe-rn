38ca79150bac66771709ef43a94d844c6de65a6c
Douglas Juarez <dfjuarezreyes21@gmail.com>
2023-11-28 07:54:59 -0600

    update card services

--- src/services/card/card.ts
+++ src/services/card/card.ts
@@ -9,3 +9,9 @@ export const getCardsService = async (token: string) : Promise<ApiResponse<Card[
     return api.get('/cards');
 };
 
+export const updateCardService = async (token: string, cardId: string, card: Card) : Promise<ApiResponse<Card>> => {
+  api.setHeader(
+      'Authorization', "Bearer "+token
+    )
+  return api.put(`/cards/${cardId}`, { id: card.id, name: card.name, last_numbers: card.last_numbers, verified: card.verified, active: card.active });
+};
