c5a5fe1b828d237d1feeb862751d43d45cb49ada
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-01-08 10:16:52 -0600

    change props currentbillingButton

--- src/components/CurrentBillingButton.tsx
+++ src/components/CurrentBillingButton.tsx
@@ -3,82 +3,96 @@ import React from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 
 import ReceiptIcon from '../assets/receipt_edit.svg';
+import ArrowRightIcon from '../assets/arrow_right_blue.svg';
 
 import {colors} from '../styles/colors';
-import {Order, Transaction} from '../model/Order';
+import {BillEntity, BillType, Order, Transaction} from '../model/Order';
 
 type CurrentBillingButtonProps = {
   isOrderDetail?: boolean;
-  currentBilling?: string;
-  order?: Order;
+  billType?: string;
+  billEntity?: string;
+  dui?: string;
+  iva?: string;
   onPress?: () => void;
 };
 
 type BillingFormatRenderProps = {
-  transaction?: Transaction | null;
-};
-
-const BillingFormatRender = ({transaction}: BillingFormatRenderProps) => {
-  return (
-    <>
-      {transaction?.bill_type === 'final' ? (
-        <>
-          <Text
-            style={styles.billingText}
-            numberOfLines={1}
-            lineBreakMode="tail">
-            Consumidor final
-          </Text>
-
-          <Text
-            style={styles.billingText}
-            numberOfLines={1}
-            lineBreakMode="tail">
-            DUI: {transaction?.dui}
-          </Text>
-        </>
-      ) : (
-        <>
-          <Text
-            style={styles.billingText}
-            numberOfLines={1}
-            lineBreakMode="tail">
-            Crédito fiscal
-          </Text>
-
-          <Text
-            style={styles.billingText}
-            numberOfLines={1}
-            lineBreakMode="tail">
-            Registro de IVA: {transaction?.iva}
-          </Text>
-          <Text
-            style={styles.billingText}
-            numberOfLines={1}
-            lineBreakMode="tail">
-            Tipo de Persona:{' '}
-            {transaction?.bill_entity === 'natural' ? 'Natural' : 'Jurídica'}
-          </Text>
-          {transaction?.bill_entity === 'natural' ? (
-            <Text
-              style={styles.billingText}
-              numberOfLines={1}
-              lineBreakMode="tail">
-              DUI: {transaction?.dui}
-            </Text>
-          ) : null}
-        </>
-      )}
-    </>
-  );
+  //transaction?: Transaction | null;
+  bill_type: string;
+  bill_entity: string;
+  dui: string;
+  iva: string;
 };
 
 export const CurrentBillingButton = ({
   isOrderDetail = false,
-  currentBilling,
-  order,
+  billType,
+  billEntity,
+  iva,
+  dui,
   onPress,
 }: CurrentBillingButtonProps) => {
+  const BillingFormatRender = ({
+    bill_type,
+    bill_entity,
+    dui,
+    iva,
+  }: BillingFormatRenderProps) => {
+    return (
+      <>
+        {bill_type === 'final' ? (
+          <>
+            <Text
+              style={styles.billingText}
+              numberOfLines={1}
+              lineBreakMode="tail">
+              Consumidor final
+            </Text>
+
+            <Text
+              style={styles.billingText}
+              numberOfLines={1}
+              lineBreakMode="tail">
+              DUI: {dui}
+            </Text>
+          </>
+        ) : (
+          <>
+            <Text
+              style={styles.billingText}
+              numberOfLines={1}
+              lineBreakMode="tail">
+              Crédito fiscal
+            </Text>
+
+            <Text
+              style={styles.billingText}
+              numberOfLines={1}
+              lineBreakMode="tail">
+              Registro de IVA: {iva}
+            </Text>
+            <Text
+              style={styles.billingText}
+              numberOfLines={1}
+              lineBreakMode="tail">
+              Tipo de Persona:{' '}
+              {bill_entity === 'natural' ? 'Natural' : 'Jurídica'}
+            </Text>
+            {bill_entity === 'natural' ? (
+              <Text
+                style={styles.billingText}
+                numberOfLines={1}
+                lineBreakMode="tail">
+                DUI: {dui}
+              </Text>
+            ) : null}
+          </>
+        )}
+      </>
+    );
+  };
+
   return (
     <Pressable onPress={onPress}>
       <View style={styles.container}>
@@ -97,12 +111,18 @@ export const CurrentBillingButton = ({
               style={styles.billingText}
               numberOfLines={1}
               lineBreakMode="tail">
-              {currentBilling}
+              {billType === 'final' ? 'Consumidor final' : 'Crédito fiscal'}
             </Text>
           ) : (
-            <BillingFormatRender transaction={order?.transaction} />
+            <BillingFormatRender
+              bill_type={billType ?? ''}
+              bill_entity={billEntity ?? ''}
+              dui={dui ?? ''}
+              iva={iva ?? ''}
+            />
           )}
         </View>
+        {!isOrderDetail ? <ArrowRightIcon height={25} width={25} /> : null}
       </View>
     </Pressable>
   );
@@ -121,6 +141,7 @@ const styles = StyleSheet.create({
     paddingRight: 23,
   },
   billingContainer: {
+    flex: 1,
     rowGap: 8,
   },
   billingText: {
