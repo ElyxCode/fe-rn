61efbb56cd0020e07833b6806eba9453ee0f955a
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-01-15 07:38:28 -0600

    bank model added

---
+++ src/model/bank.ts
@@ -0,0 +1,9 @@
+export interface Bank {
+    id:          number;
+    name:        string;
+    type:        string;
+    number:      string;
+    bank:        string;
+    person_type: string;
+    avatar:      string;
+}
