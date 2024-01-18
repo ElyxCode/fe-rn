44e907be0c9162d26f10e46a5185d448508d4256
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-01-18 17:11:23 -0600

    set new value state and method for handle

--- src/services/card/cardSlice.ts
+++ src/services/card/cardSlice.ts
@@ -2,43 +2,51 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { Card } from '../../model/Card';
 
+type CurrentCard = {
+    card: Card;
+    cardConfirmAdded: Card;
+}
 
-const initialState: Card = {
-    id: -1,
-    name: '',
-    last_numbers: '',
-    month: '',
-    year: '',
-    verified: false,
-    active: false,
+const initialState: CurrentCard = {
+    card: {
+        id: -1,
+        name: '',
+        last_numbers: '',
+        month: '',
+        year: '',
+        verified: false,
+        active: false,
+    },
+    cardConfirmAdded: {
+        id: -1,
+        name: '',
+        last_numbers: '',
+        month: '',
+        year: '',
+        verified: false,
+        active: false,
+    }
 };
 
 export const cardSlice = createSlice({
     name: 'currentCard',
     initialState: initialState,
     reducers: {
-        setCard: (state, action: PayloadAction<Card>) => {
-            state.id = action.payload.id;         
-            state.last_numbers = action.payload.last_numbers;         
-            state.name = action.payload.name;         
-            state.month = action.payload.month;
-            state.year =  action.payload.year;         
-            state.active = action.payload.active;
-            state.verified = action.payload.verified;         
+        setCard: (state, action: PayloadAction<Card>) => {         
+            state.card = action.payload       
         },
-        clearCard: (state) => {
-           state.id = initialState.id;
-           state.last_numbers = initialState.last_numbers;
-           state.name = initialState.name;
-           state.month = initialState.month;
-           state.year = initialState.year;
-           state.active = initialState.active;
-           state.verified = initialState.verified;
-           
+        setCardConfirmAdded: (state, action: PayloadAction<Card>) => {
+            state.cardConfirmAdded = action.payload        
+        },
+        clearCard: (state) => {     
+            state.card = initialState.card
+        },
+        clearCardConfirmAdded: (state) => {
+            state.cardConfirmAdded = initialState.card
         }
     },
   });
 
-  export const { setCard, clearCard } = cardSlice.actions;
+  export const { setCard, setCardConfirmAdded, clearCard, clearCardConfirmAdded } = cardSlice.actions;
 
   export default cardSlice.reducer;
