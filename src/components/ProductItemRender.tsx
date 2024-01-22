6526df4d64f9da7f7e36e828be74a5d6df4b35c8
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-01-22 17:11:31 -0600

    set new prop for component for avoid reset category selected

--- src/components/ProductItemRender.tsx
+++ src/components/ProductItemRender.tsx
@@ -1,93 +1,108 @@
-import { ProductProps } from "../model/ProductProps";
-import {Image, StyleSheet, Text, View} from 'react-native'
-import { colors } from "../styles/colors";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
-import {Product} from '../model/product'
+import {Image, StyleSheet, Text, View} from 'react-native';
 
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
+import {ProductProps} from '../model/ProductProps';
 
-export const ProductItemRender = ({product, navigation}:ProductProps ) => {
+import {colors} from '../styles/colors';
 
-
-
-    return (
-      <TouchableOpacity style={styles.productContainer} onPress={() => {navigation.navigate('ProductDetailScreen',  { ProductProps:{product,navigation}} )}}>
-        <View style={styles.imageProductContainer}>
-          <Image source={{uri: product.image}} style={{height: 80, width: 80}} />
-        </View>
-        <View style={styles.productInfoContainer}>
+export const ProductItemRender = ({
+  product,
+  navigation,
+  setOpenModal,
+}: ProductProps) => {
+  return (
+    <TouchableOpacity
+      style={styles.productContainer}
+      onPress={() => {
+        setOpenModal(true);
+        navigation.navigate('ProductDetailScreen', {
+          ProductProps: {product, navigation},
+        });
+      }}>
+      <View style={styles.imageProductContainer}>
+        <Image source={{uri: product.image}} style={{height: 80, width: 80}} />
+      </View>
+      <View style={styles.productInfoContainer}>
+        <Text
+          style={styles.productNameText}
+          numberOfLines={1}
+          lineBreakMode="tail">
+          {product.name}
+        </Text>
+        <View style={styles.priceProductContainer}>
           <Text
-            style={styles.productNameText}
-            numberOfLines={1}
-            lineBreakMode="tail">
-            {product.name}
+            style={[
+              styles.priceText,
+              {
+                textDecorationLine: product.price_with_discount
+                  ? 'line-through'
+                  : 'none',
+                color: product.price_with_discount
+                  ? colors.DarkGrayColor
+                  : colors.PrimaryTextColor,
+              },
+            ]}>
+            ${product.price}
           </Text>
-          <View style={styles.priceProductContainer}>
+          {product.price_with_discount ? (
             <Text
               style={[
                 styles.priceText,
                 {
-                  textDecorationLine: product.price_with_discount ? 'line-through' : 'none',
+                  textDecorationLine: product.price_with_discount
+                    ? 'none'
+                    : 'line-through',
                   color: product.price_with_discount
-                    ? colors.DarkGrayColor
-                    : colors.PrimaryTextColor,
+                    ? colors.PrimaryTextColor
+                    : colors.DarkGrayColor,
                 },
               ]}>
-              ${product.price}
+              ${product.price_with_discount}
             </Text>
-            {product.price_with_discount ? (
-              <Text
-                style={[
-                  styles.priceText,
-                  {
-                    textDecorationLine: product.price_with_discount ? 'none' : 'line-through',
-                    color: product.price_with_discount
-                      ? colors.PrimaryTextColor
-                      : colors.DarkGrayColor,
-                  },
-                ]}>
-                ${product.price_with_discount}
-              </Text>
-            ) : null}
-          </View>
-          <Text style={styles.brandText}>{product.brand.name}</Text>
+          ) : null}
         </View>
-      </TouchableOpacity>
-    );
-  };
+        <Text style={styles.brandText}>{product.brand.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-  const styles = StyleSheet.create({
-    productContainer: {
-        flexDirection: 'row',
-        backgroundColor: colors.White,
-        borderRadius: 10,
-        marginHorizontal: 20,
-        padding: 5,
-      },imageProductContainer: {
-        height: 80,
-      }, productInfoContainer: {
-        justifyContent: 'space-between',
-        paddingLeft: 20,
-        paddingVertical: 5,
-        width: 250,
-      },productNameText: {
-        
-        fontSize: 12,
-        color: colors.DarkGrayColor,
-        fontFamily: 'Poppins-Medium',
-      },priceProductContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        columnGap: 10,
-      }, priceText: {
-        fontSize: 12,
-        color: colors.DarkGrayColor,
-        fontFamily: 'Poppins-Medium',
-      },
-      brandText: {
-        fontSize: 12,
-        color: colors.PrimaryTextColor,
-        fontFamily: 'Poppins-Medium',
-      },
-  })
+const styles = StyleSheet.create({
+  productContainer: {
+    flexDirection: 'row',
+    backgroundColor: colors.White,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    padding: 5,
+  },
+  imageProductContainer: {
+    height: 80,
+  },
+  productInfoContainer: {
+    justifyContent: 'space-between',
+    paddingLeft: 20,
+    paddingVertical: 5,
+    width: 250,
+  },
+  productNameText: {
+    fontSize: 12,
+    color: colors.DarkGrayColor,
+    fontFamily: 'Poppins-Medium',
+  },
+  priceProductContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 10,
+  },
+  priceText: {
+    fontSize: 12,
+    color: colors.DarkGrayColor,
+    fontFamily: 'Poppins-Medium',
+  },
+  brandText: {
+    fontSize: 12,
+    color: colors.PrimaryTextColor,
+    fontFamily: 'Poppins-Medium',
+  },
+});
