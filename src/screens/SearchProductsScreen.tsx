ae50253d3c24972943979a892e64be1e867ac9d4
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-01-28 09:18:58 -0600

    fix get product from services and load more products

--- src/screens/SearchProductsScreen.tsx
+++ src/screens/SearchProductsScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   FlatList,
@@ -12,33 +12,42 @@ import {CustomNavBar} from '../components/CustomNavBar';
 import {ProductItemRender} from '../components/ProductItemRender';
 import {SearchInput} from '../components/SearchInput';
 
+import {LoaderScreen} from './LoaderScreen';
+
 import {
   nextPageProductsService,
+  productsService,
   searchProductByProductName,
 } from '../services/product/product';
 
-import {ProductResponse} from '../model/product';
+import {Product} from '../model/product';
 
 import {colors} from '../styles/colors';
 
 export const SearchProductsScreen = ({route, navigation}: any) => {
-  const {branchData, productResponse} = route.params;
-
+  const {branchData} = route.params;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState('');
+  const [products, setProducts] = useState<Product[]>([]);
   const [loadMore, setLoadMore] = useState<boolean>(false);
   const [nextPageProduct, setNextPageProduct] = useState<
     string | null | undefined
   >('');
-  const [searchProductResponse, setSearchProductResponse] =
-    useState<ProductResponse>(productResponse);
 
-  const textChanged = async (text: string) => {
-    setInputValue(text);
-  };
+  useEffect(() => {
+    getProductData();
+  }, []);
 
-  const clearText = () => {
-    console.log('borrando');
-    setInputValue('');
+  const getProductData = async () => {
+    setIsLoading(true);
+    const response = await productsService(branchData.id);
+    if (response.ok) {
+      setNextPageProduct(response.data?.links.next);
+      setProducts(response.data?.data as Product[]);
+    } else {
+      console.log({error: response.originalError});
+    }
+    setIsLoading(false);
   };
 
   // carga mas productos
@@ -53,7 +62,8 @@ export const SearchProductsScreen = ({route, navigation}: any) => {
 
     if (response.ok) {
       setNextPageProduct(response.data?.links.next);
-      setSearchProductResponse(response.data as ProductResponse);
+
+      setProducts([...products, ...(response.data?.data ?? [])]);
     } else {
       console.log({error: response.originalError});
     }
@@ -62,72 +72,88 @@ export const SearchProductsScreen = ({route, navigation}: any) => {
   };
 
   const searchProducts = async () => {
-    setLoadMore(true);
+    setIsLoading(true);
     const response = await searchProductByProductName(
       branchData.id,
       inputValue,
     );
 
     if (response.ok) {
-      setSearchProductResponse(response?.data as ProductResponse);
+      setProducts(response.data?.data ?? []);
     }
-    setLoadMore(false);
+    setIsLoading(false);
+  };
+
+  const textChanged = async (text: string) => {
+    setInputValue(text);
+  };
+
+  const clearText = () => {
+    if (isLoading) return;
+    console.log('borrando');
+    setInputValue('');
+    getProductData();
   };
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <CustomNavBar />
-      <View style={styles.container}>
-        <FlatList
-          ListHeaderComponent={
-            <SearchInput
-              textChanged={e => textChanged(e)}
-              onPressCloseIcon={clearText}
-              inputValue={inputValue}
-              title="Buscar Producto"
-              onSubmit={searchProducts}
-            />
-          }
-          data={searchProductResponse?.data}
-          contentContainerStyle={{
-            paddingBottom: 20,
-          }}
-          renderItem={({item}) => (
-            <ProductItemRender product={item} navigation={navigation} />
-          )}
-          showsVerticalScrollIndicator={false}
-          scrollEnabled={!loadMore}
-          ItemSeparatorComponent={() => <View style={{height: 15}}></View>}
-          keyExtractor={item => item.id.toString() + Math.random() * 3}
-          initialNumToRender={20}
-          onEndReachedThreshold={0.001}
-          onEndReached={() => {
-            if (
-              nextPageProduct !== null &&
-              nextPageProduct !== '' &&
-              nextPageProduct !== undefined
-            ) {
-              loadMoreProducts();
-            } else {
-              console.log('No hay mas items');
-            }
-          }}
-          ListFooterComponent={
-            loadMore ? (
-              <>
-                <View
-                  style={{
-                    height: 25,
-                    width: '100%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <ActivityIndicator size={25} color={colors.PrimaryColor} />
-                </View>
-              </>
-            ) : null
-          }
+      <View style={{paddingHorizontal: 20}}>
+        <SearchInput
+          textChanged={e => textChanged(e)}
+          onPressCloseIcon={clearText}
+          inputValue={inputValue}
+          title="Buscar Producto"
+          onSubmit={searchProducts}
+          editable={!isLoading}
         />
+      </View>
+      <View style={styles.container}>
+        {isLoading ? (
+          <LoaderScreen />
+        ) : (
+          <FlatList
+            data={products}
+            contentContainerStyle={{
+              paddingBottom: 20,
+            }}
+            renderItem={({item}) => (
+              <ProductItemRender product={item} navigation={navigation} />
+            )}
+            showsVerticalScrollIndicator={false}
+            scrollEnabled={!loadMore}
+            ItemSeparatorComponent={() => <View style={{height: 15}}></View>}
+            keyExtractor={item => item.id.toString() + Math.random() * 3}
+            initialNumToRender={20}
+            onEndReachedThreshold={0.001}
+            onEndReached={() => {
+              if (
+                nextPageProduct !== null &&
+                nextPageProduct !== '' &&
+                nextPageProduct !== undefined
+              ) {
+                loadMoreProducts();
+              } else {
+                console.log('No hay mas items');
+              }
+            }}
+            ListFooterComponent={
+              loadMore ? (
+                <>
+                  <View
+                    style={{
+                      height: 25,
+                      width: '100%',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <ActivityIndicator size={25} color={colors.PrimaryColor} />
+                  </View>
+                </>
+              ) : null
+            }
+          />
+        )}
       </View>
     </SafeAreaView>
   );
