a9fc90b2b4431fae15e0590c639dc5e0953c5f7e
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-02-21 08:47:05 -0600

    formating and change resize mode image

--- src/components/PromotionImageDetail.tsx
+++ src/components/PromotionImageDetail.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import {Image, StyleProp, Text, View, ViewStyle} from 'react-native'
+import React from 'react';
+import {Image} from 'react-native';
 
-type imageProps={
-    image:string,
-    height: number,
-    
-    customStyles?: StyleProp<ViewStyle>;
-}
+type imageProps = {
+  image: string;
+  height: number;
+};
 
-export const PromotionImageDetail = ({image,height, customStyles}:imageProps) =>{
-    return(
-        <>
-         <Image
-          resizeMode="contain"
-          source={{uri: image}}
-          height={height}
-          style={{borderRadius: 12, marginHorizontal:20}}
-        />
-         
-        </>
-    )
-}
+export const PromotionImageDetail = ({image, height}: imageProps) => {
+  return (
+    <Image
+      resizeMode="stretch"
+      source={{uri: image}}
+      height={height}
+      style={{
+        borderRadius: 12,
+        marginHorizontal: 20,
+      }}
+    />
+  );
+};
