e5c6410779133e0b27164b2d9048d5d5a930e229
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-02-21 09:10:59 -0600

    align items container

--- src/components/PlaceCell.tsx
+++ src/components/PlaceCell.tsx
@@ -1,38 +1,41 @@
-import React from "react";
-import { GooglePlaceAutoCompletePrediction } from "../model/GooglePlaceAutoCompleteResult";
-import {TouchableOpacity, View, Text, StyleSheet,} from 'react-native';
-import SearchIcon from '../assets/search_home.svg'
+import React from 'react';
+import {GooglePlaceAutoCompletePrediction} from '../model/GooglePlaceAutoCompleteResult';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import SearchIcon from '../assets/search_home.svg';
 
 type Props = {
-    item: GooglePlaceAutoCompletePrediction;
-    
-    onPress: () => void;
-  };
+  item: GooglePlaceAutoCompletePrediction;
 
-export const PlaceCell = ({item, onPress}:Props) =>{
-    return(
-      <>
+  onPress: () => void;
+};
+
+export const PlaceCell = ({item, onPress}: Props) => {
+  return (
+    <>
       <TouchableOpacity onPress={onPress}>
         <View style={styles.container}>
-        <SearchIcon height={24} width={24}/>
-         <Text  style={styles.text}>{item.structured_formatting.main_text}</Text>
+          <SearchIcon height={24} width={24} />
+          <Text style={styles.text}>
+            {item.structured_formatting.main_text}
+          </Text>
         </View>
-        </TouchableOpacity>
-      </>
-    );
-}
+      </TouchableOpacity>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-container:{
-    borderRadius:10,
-    flexDirection:'row',
-    backgroundColor:'white',
-    paddingHorizontal:15,
-    paddingVertical:12,
-    marginVertical:8
-
-},text:{
-  paddingHorizontal:16,
-  fontSize:12
-}
-})
+  container: {
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    marginVertical: 8,
+    alignItems: 'center',
+  },
+  text: {
+    paddingHorizontal: 16,
+    fontSize: 12,
+  },
+});
