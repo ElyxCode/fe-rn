891b06dde1cfc1d9c46a03964eff6cb447280561
Douglas Juarez <dfjuarezreyes21@gmail.com>
2024-02-26 11:08:04 -0600

    fix navigation for singup navigation

--- src/navigator/UnloggedNavigation.tsx
+++ src/navigator/UnloggedNavigation.tsx
@@ -3,11 +3,17 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {LoginScreen} from '../screens/LoginScreen';
 import {SignUpScreen} from '../screens/SignUpScreen';
 import {OptionsUnLoggedScreen} from '../screens/OptionsUnLoggedScreen';
+import {SignUpComplementScreen} from '../screens/SignUpComplementScreen';
+import {SignUpWelcomeScreen} from '../screens/SignUpWelcomeScreen';
+import {BiometricDetailScreen} from '../screens/BiometricDetailScreen';
 
 export type UnloggedStackParams = {
   LoginScreen: undefined;
   SignUpScreen: undefined;
   OptionsUnLoggedScreen: undefined;
+  SignUpComplementScreen: undefined;
+  SignUpWelcomeScreen: undefined;
+  BiometricDetailScreen: undefined;
 };
 
 const UnloggedStack = createStackNavigator<UnloggedStackParams>();
@@ -21,6 +27,18 @@ export const UnloggedNavigation = () => {
       />
       <UnloggedStack.Screen name="LoginScreen" component={LoginScreen} />
       <UnloggedStack.Screen name="SignUpScreen" component={SignUpScreen} />
+      <UnloggedStack.Screen
+        name="SignUpComplementScreen"
+        component={SignUpComplementScreen}
+      />
+      <UnloggedStack.Screen
+        name="SignUpWelcomeScreen"
+        component={SignUpWelcomeScreen}
+      />
+      <UnloggedStack.Screen
+        name="BiometricDetailScreen"
+        component={BiometricDetailScreen}
+      />
     </UnloggedStack.Navigator>
   );
 };
